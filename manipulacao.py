474e16d6c6b942a108b281d127ae3a098f512177
lvr44 <105324948+lvr44@users.noreply.github.com>
2022-05-13 20:04:29 -0300

    Update manipulacao.py

--- manipulacao.py
+++ manipulacao.py
@@ -1,1 +1,14 @@
+# Módulo de manipulação de strings e arquivos
 
+
+# Função de acesso que recebe um arquivo e retorna uma string com os dados do arquivo
+def fileToString(arquivo):
+  
+  return
+
+
+# Função de acesso que recebe uma string e um arquivo e retorna o arquivo com a string gravada
+def stringToFile(string, arquivo):
+  
+  
+  return 
