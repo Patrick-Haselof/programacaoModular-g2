467056d9905172acee9a0008845f5d6654be98d0
Patrick-Haselof <82117539+Patrick-Haselof@users.noreply.github.com>
2022-05-16 14:45:03 -0300

    Update substituicao.py

--- substituicao.py
+++ substituicao.py
@@ -1,4 +1,26 @@
 # módulo de substituição de palavras em strings
 
+'''
+#prototipo em andamento (necessita de adaptacao para uso de case match)
+
+def replace_word_in_text_1(txt,w,s):
+    x = txt.find("hello")
+    
+    if(x == -1):
+        print("[ERROR]: Selected word is not in text, returning original text")
+
+    else:
+        counter = 1
+        while(1):
+            y = txt.find(w,x+len(w),len(txt))
+            if(y == -1):
+                txt = txt.replace(w,s,counter)
+                return txt
+            x = y
+            counter += 1
+    return txt
+'''
+
+
 def substitui_palavra(texto,palavra_sub):
     return "Teste mock"
